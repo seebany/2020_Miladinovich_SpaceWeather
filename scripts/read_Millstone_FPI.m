a9bc30ea7ef5a33cfda98a3e3532fedc2882381d
seebany <36240722+seebany@users.noreply.github.com>
2020-05-03 21:34:55 -0500

    Create read_Millstone_FPI.m

---
+++ scripts/read_Millstone_FPI.m
@@ -0,0 +1,34 @@
+% This function reads in FPI data files of the form Millstone_FPI_yyyymmdd.txt
+% obtained of the Millstone Hill high-resolution FPI from the CEDAR Madrigal web
+site
+% The function supports script plot_Fig1b.m for producing Figure 1b of 
+% Miladinovich et al., (2020).
+%
+% See AUTHORS, LICENSE, and README files for additional information.
+% Seebany Datta-Barua
+% Illinois Institute of Technology
+% sdattaba@iit.edu
+% 16 Apr 2020
+
+function [uwut, azm, elm, gdalt, vnu, dvnu, wind_err] = read_Millstone_FPI(filen
+ame)
+
+% Load file into matrix S.
+S=load(filename);
+
+% Each column in S corresponds to the following.
+temp = 'YEAR,MONTH,DAY,HOUR,MINUTE,SECOND,GDALT,AZM,ELM,VNU,DVNU,WIND_ERR';
+temp = textscan(temp,'%s%s%s%s%s%s%s%s%s%s%s%s','Delimiter', ',');
+    
+bad=find(isnan(S(:,10)));
+        
+S(bad, :)=[];
+
+% Assign a variable name to each column e.g., year = S(:,1);
+for i=1:length(temp)
+    command = [lower(char(temp{i})), '=', 'S(:, i);' ];
+    eval(command);
+end
+
+uwut = datenum([year month day hour minute second]);
+clear temp command
